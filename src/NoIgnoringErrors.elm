993fb7dd6d7657d6ccdfa1fdd86921bda853053d
Jeroen Engels <jfm.engels@gmail.com>
2021-01-08 15:09:09 +0100

    Implement NoIgnoringErrors rule

--- src/NoIgnoringErrors.elm
+++ src/NoIgnoringErrors.elm
@@ -6,10 +6,13 @@ module NoIgnoringErrors exposing (rule)
 
 -}
 
-import Review.Rule as Rule exposing (Rule)
+import Elm.Syntax.Expression as Expression exposing (Expression)
+import Elm.Syntax.Node as Node exposing (Node)
+import Elm.Syntax.Pattern as Pattern
+import Review.Rule as Rule exposing (Error, Rule)
 
 
-{-| Reports... REPLACEME
+{-| Reports when error details are not being used.
 
     config =
         [ NoIgnoringErrors.rule
@@ -18,20 +21,45 @@ import Review.Rule as Rule exposing (Rule)
 
 ## Fail
 
-    a =
-        "REPLACEME example to replace"
+    notificationMessage =
+        case foo of
+            Ok () ->
+                "Success!"
+
+            Err _ ->
+                "Failure!"
 
 
 ## Success
 
-    a =
-        "REPLACEME example to replace"
+    notificationMessage =
+        case foo of
+            Ok () ->
+                "Success!"
+
+            Err errorMessage ->
+                "Failed to make this work because " ++ errorMessage
+
+As shown in the following example, the rule will only check whether a wildcard (`_`) is used for `Err`. I recommend enabling the
+[`NoUnused.Patterns`](https://package.elm-lang.org/packages/jfmengels/elm-review-unused/latest/NoUnused-Patterns) rule
+to help get you to the state where the noise is removed.
+
+    notificationMessage =
+        case foo of
+            Ok () ->
+                "Success!"
+
+            Err errorMessage ->
+                -- `errorMessage` is not used, but the rule only checks whether a wildcard is being used.
+                "Failure!"
 
 
 ## When (not) to enable this rule
 
-This rule is useful when REPLACEME.
-This rule is not useful when REPLACEME.
+This rule is still experimental. I am trying to figure out if this error is always useful or how to tweak it to remove
+false positives and discover more cases where errors are ignored.
+
+I would recommend at this point to run this rule to find places to improve but not to add it to your configuration.
 
 
 ## Try it out
@@ -46,5 +74,55 @@ elm-review --template jfmengels/elm-review-no-ignoring-errors/example --rules No
 rule : Rule
 rule =
     Rule.newModuleRuleSchema "NoIgnoringErrors" ()
-        -- Add your visitors
+        |> Rule.withExpressionEnterVisitor expressionVisitor
         |> Rule.fromModuleRuleSchema
+
+
+type alias Context =
+    ()
+
+
+expressionVisitor : Node Expression -> Context -> ( List (Error {}), Context )
+expressionVisitor node context =
+    case Node.value node of
+        Expression.CaseExpression { cases } ->
+            ( List.concatMap (\( pattern, _ ) -> patternProblems pattern) cases
+            , context
+            )
+
+        _ ->
+            ( [], context )
+
+
+patternProblems : Node Pattern.Pattern -> List (Error {})
+patternProblems node =
+    case Node.value node of
+        Pattern.NamedPattern { moduleName, name } patterns ->
+            if name == "Err" && List.map Node.value patterns == [ Pattern.AllPattern ] then
+                Rule.error
+                    { message = "The error is being ignored."
+                    , details = [ "Please check whether the error can't be used to improve the situation for the user. You can for instance display the error message to the user or re-attempt the operation." ]
+                    }
+                    (Node.range node)
+                    :: List.concatMap patternProblems patterns
+
+            else
+                List.concatMap patternProblems patterns
+
+        Pattern.ParenthesizedPattern pattern ->
+            patternProblems pattern
+
+        Pattern.UnConsPattern left right ->
+            patternProblems left ++ patternProblems right
+
+        Pattern.ListPattern patterns ->
+            List.concatMap patternProblems patterns
+
+        Pattern.TuplePattern patterns ->
+            List.concatMap patternProblems patterns
+
+        Pattern.AsPattern pattern _ ->
+            patternProblems pattern
+
+        _ ->
+            []
--- tests/NoIgnoringErrorsTest.elm
+++ tests/NoIgnoringErrorsTest.elm
@@ -5,20 +5,136 @@ import Review.Test
 import Test exposing (Test, describe, test)
 
 
+message : String
+message =
+    "The error is being ignored."
+
+
+details : List String
+details =
+    [ "Please check whether the error can't be used to improve the situation for the user. You can for instance display the error message to the user or re-attempt the operation." ]
+
+
 all : Test
 all =
     describe "NoIgnoringErrors"
-        [ test "should report an error when REPLACEME" <|
+        [ test "should report an error when the argument to Err is not used" <|
             \() ->
                 """module A exposing (..)
-a = 1
+a =
+  case foo of
+      Ok () -> 1
+      Err _ -> 1
 """
                     |> Review.Test.run rule
                     |> Review.Test.expectErrors
                         [ Review.Test.error
-                            { message = "REPLACEME"
-                            , details = [ "REPLACEME" ]
-                            , under = "REPLACEME"
+                            { message = message
+                            , details = details
+                            , under = "Err _"
+                            }
+                        ]
+        , test "should not report an error when the argument to Err is used" <|
+            \() ->
+                """module A exposing (..)
+a =
+  case foo of
+      Ok () -> 1
+      Err () -> 1
+"""
+                    |> Review.Test.run rule
+                    |> Review.Test.expectNoErrors
+        , test "should report an error when the wildcard with error is in a parens" <|
+            \() ->
+                """module A exposing (..)
+a =
+  case foo of
+      (Ok ()) -> 1
+      (Err _) -> 1
+"""
+                    |> Review.Test.run rule
+                    |> Review.Test.expectErrors
+                        [ Review.Test.error
+                            { message = message
+                            , details = details
+                            , under = "Err _"
+                            }
+                        ]
+        , test "should report an error when the wildcard with error is nested" <|
+            \() ->
+                """module A exposing (..)
+a =
+  case foo of
+      Just (Ok ()) -> 1
+      Just (Err _) -> 1
+      Nothing -> 1
+"""
+                    |> Review.Test.run rule
+                    |> Review.Test.expectErrors
+                        [ Review.Test.error
+                            { message = message
+                            , details = details
+                            , under = "Err _"
+                            }
+                        ]
+        , test "should report an error when the wildcard with error is :: to a list" <|
+            \() ->
+                """module A exposing (..)
+a =
+  case foo of
+      Err _ :: list -> 1
+"""
+                    |> Review.Test.run rule
+                    |> Review.Test.expectErrors
+                        [ Review.Test.error
+                            { message = message
+                            , details = details
+                            , under = "Err _"
+                            }
+                        ]
+        , test "should report an error when the wildcard with error is in a list" <|
+            \() ->
+                """module A exposing (..)
+a =
+  case foo of
+      [Err _] -> 1
+"""
+                    |> Review.Test.run rule
+                    |> Review.Test.expectErrors
+                        [ Review.Test.error
+                            { message = message
+                            , details = details
+                            , under = "Err _"
+                            }
+                        ]
+        , test "should report an error when the wildcard with error is in a tuple" <|
+            \() ->
+                """module A exposing (..)
+a =
+  case foo of
+      (_, Err _) -> 1
+"""
+                    |> Review.Test.run rule
+                    |> Review.Test.expectErrors
+                        [ Review.Test.error
+                            { message = message
+                            , details = details
+                            , under = "Err _"
+                            }
+                        ]
+        , test "should report an error when the wildcard with error is aliased" <|
+            \() ->
+                """module A exposing (..)
+a =
+  case foo of
+      ((Err _) as error) -> 1
+"""
+                    |> Review.Test.run rule
+                    |> Review.Test.expectErrors
+                        [ Review.Test.error
+                            { message = message
+                            , details = details
+                            , under = "Err _"
                             }
                         ]
         ]
